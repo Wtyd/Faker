d5b3e09c9ed9159c41a9372b6b28eb1c6956ff52
Jorge Lilao <jorgelilaoc@gmail.com>
2024-02-22 16:08:19 +0100

    fix (Doctrine backward-compatibility) Fixed conditional to avoid raise warning when old interface exists

--- src/Faker/ORM/Doctrine/backward-compatibility.php
+++ src/Faker/ORM/Doctrine/backward-compatibility.php
@@ -2,10 +2,10 @@
 
 declare(strict_types=1);
 
-if (!class_exists('Doctrine\Common\Persistence\Mapping\ClassMetadata')) {
+if (!interface_exists('Doctrine\Common\Persistence\Mapping\ClassMetadata')) {
     class_alias(\Doctrine\Persistence\Mapping\ClassMetadata::class, 'Doctrine\Common\Persistence\Mapping\ClassMetadata');
 }
 
-if (!class_exists('Doctrine\Common\Persistence\ObjectManager')) {
+if (!interface_exists('Doctrine\Common\Persistence\ObjectManager')) {
     class_alias(\Doctrine\Persistence\ObjectManager::class, 'Doctrine\Common\Persistence\ObjectManager');
 }
